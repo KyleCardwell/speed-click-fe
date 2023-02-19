027316af2fcfddeb6887ba49f696d43887dcdf75
Kyle Cardwell <kyle.e.cardwell@gmail.com>
2023-02-19 10:26:21 -0700

    randomly selects starting boxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import SpeedBox from "./components/SpeedBox";
@@ -38,6 +38,28 @@ const boxes: gameBox[] = [
 
 function App() {
 	const [speedBoxes, setSpeedBoxes] = useState<gameBox[]>(boxes);
+  const [checkedBoxes, setCheckedBoxes] = useState<Set<number>>()
+
+  const boxesToCheck = new Set<number>()
+
+  useEffect(() => {
+    while (boxesToCheck.size < 8) {
+      const num: number = Math.floor(25 * Math.random())
+      boxesToCheck.add(num)
+    }
+  
+    setCheckedBoxes(boxesToCheck)
+
+  }, [])
+
+  useEffect(() => {
+    setSpeedBoxes(speedBoxes.map(function(speedBox: gameBox, index: number): gameBox {
+      if (checkedBoxes?.has(index)) {
+        speedBox.clickToWin = true
+      }
+      return speedBox
+    }))
+  }, [checkedBoxes])
 
   const handleClick = (num: number) => {
     setSpeedBoxes(speedBoxes.map(function(speedBox: gameBox, index: number): gameBox {
@@ -49,17 +71,21 @@ function App() {
   }
 
 
+
+
+
 	return (
 		<div className="App">
 			<div className="gameContainer">
 				<div className="gameBoard">
 					{speedBoxes.map((box, index) => {
-						console.log("box", box);
+						// console.log("box", box);
 						return (
 							<SpeedBox
 								clickToWin={box.clickToWin}
 								toggleBox={handleClick}
 								num={index}
+                checkedBoxes={checkedBoxes}
 								key={index}
 							/>
 						);
